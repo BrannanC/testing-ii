6ed85b6edabf4838ad7734fadb249124bb86a807
BrannanC <brannanconrad@gmail.com>
2019-03-05 15:31:00 -0600

    Finished tests

--- src/baseball/Baseball.test.js
+++ src/baseball/Baseball.test.js
@@ -10,19 +10,70 @@ describe('<Baseball />', () => {
     expect(getByText(/Baseball/i))
   });
 
+  it('Strikes: 0 when strike button is clicked 3x', () => {
+    const { getByText } = render(<Baseball />);
+    const strikeButton = getByText(/strike!/i);
+
+    fireEvent.click(strikeButton);
+    fireEvent.click(strikeButton);
+    fireEvent.click(strikeButton);
+    expect(getByText(/Strikes/i)).toHaveTextContent('0');
+  });
+
   it('Adds a strike when strike button is clicked', () => {
       const { getByText } = render(<Baseball />);
       const strikeButton = getByText(/strike!/i);
 
       fireEvent.click(strikeButton);
-      expect(getByText(/Strikes: 1/i)).toBeInTheDocument();
+      expect(getByText(/Strikes/i)).toHaveTextContent('1');
   });
+
+  it('Adds a strike when foul! button is clicked', () => {
+        const { getByText } = render(<Baseball />);
+        const foulButton = getByText(/foul!/i);
+
+        fireEvent.click(foulButton);
+        expect(getByText(/Strikes/i)).toHaveTextContent('2');
+    });
+
+    it('Does not add a strike when foul! button is clicked (2 strikes)', () => {
+        const { getByText } = render(<Baseball />);
+        const foulButton = getByText(/foul!/i);
+
+        fireEvent.click(foulButton);
+        expect(getByText(/Strikes/i)).toHaveTextContent('2');
+    });
+
+    it('Balls: 0 when ball! button is clicked 4x', () => {
+        const { getByText } = render(<Baseball />);
+        const ballButton = getByText(/ball!/i);
+
+        fireEvent.click(ballButton);
+        fireEvent.click(ballButton);
+        fireEvent.click(ballButton);
+        fireEvent.click(ballButton);
+        expect(getByText(/Balls/i)).toHaveTextContent('0');
+    });
 
   it('Adds a ball when ball! button is clicked', () => {
     const { getByText } = render(<Baseball />);
     const ballButton = getByText(/ball!/i);
 
     fireEvent.click(ballButton);
-    expect(getByText(/Balls: 1/i)).toBeInTheDocument();
-});
+    expect(getByText(/Balls/i)).toHaveTextContent('1');
+  });
+
+  it('Balls and Strikes to 0 when hit! button is clicked', () => {
+    const { getByText } = render(<Baseball />);
+    const ballButton = getByText(/ball!/i);
+    const foulButton = getByText(/foul!/i);
+    const hitButton = getByText(/hit!/i);
+
+    fireEvent.click(ballButton);
+    fireEvent.click(foulButton);
+    fireEvent.click(hitButton);
+    expect(getByText(/balls/i)).toHaveTextContent('0');
+    expect(getByText(/strikes/i)).toHaveTextContent('0');
+  });
+
 });
